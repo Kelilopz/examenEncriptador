d89d0982edc17238c4010fb403e3759a71c51bdf
Kelilopz <kelilopz2171804@gmail.com>
2024-08-30 15:29:42 -0500

    feat: :sparkles: se inicia con el frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div></div>
+      <div className='Contenedor'>
+        <div className='Logo'>
+          <img src="/Logo.png" alt="" />
+        </div>
+
+        <div className='Contenedo_Texto'>
+          <p>ingrese el texto aqui</p>
+        </div>
+        <div className='Alerta'>
+          <img src="/IconoExclamacion.png" alt="" />
+          <p>Solo letras minusculas y sin acentos</p>        </div>
+
+          <div className='Botones'>
+          <a className='BotonEncriptar' href=""><button>Encriptar</button></a>
+            <a className='BotonDesencriptar' href=""><button>Desencriptar</button></a>
+          </div>
+      
+      <div className='ContenedorRespuesta'>
+        <img src="/Muñeco.png" alt="" />
+        <h2>Ningun mensaje fue encontrado</h2>
+        <p>Ingresa el texto que desees encriptar o desencriptar</p>
+      </div>
+      </div>
     </>
   )
 }
