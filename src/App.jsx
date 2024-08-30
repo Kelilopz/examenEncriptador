1ceb666c05bb705038f092c80b7b630b7efdf376
Kelilopz <kelilopz2171804@gmail.com>
2024-08-30 15:53:31 -0500

    feat: :sparkles: listo el frontend
    listo el forntend

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,10 @@ function App() {
         <div className='Logo'>
           <img src="/Logo.png" alt="" />
         </div>
-
-        <div className='Contenedo_Texto'>
-          <p>ingrese el texto aqui</p>
+        <div className='Contenedor_Cuerpo'>
+        <div className='Contenedor_Entrante'>
+        <div className='Contenedor_Texto'>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem temporibus fugit sint accusamus reiciendis optio aut sed ea, excepturi omnis distinctio quis labore dolorem corrupti quasi unde soluta sapiente voluptates!</p>
         </div>
         <div className='Alerta'>
           <img src="/IconoExclamacion.png" alt="" />
@@ -20,11 +21,12 @@ function App() {
           <a className='BotonEncriptar' href=""><button>Encriptar</button></a>
             <a className='BotonDesencriptar' href=""><button>Desencriptar</button></a>
           </div>
-      
+          </div>
       <div className='ContenedorRespuesta'>
         <img src="/Muñeco.png" alt="" />
         <h2>Ningun mensaje fue encontrado</h2>
         <p>Ingresa el texto que desees encriptar o desencriptar</p>
+      </div>
       </div>
       </div>
     </>
